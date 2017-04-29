28c74620b75c08212b21721ae802f3d5a460816c
morgansliman <morgansliman@gmail.com>
2017-04-29 04:18:26 -0400

    gulp process started

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+//  Setting up the express app
+const app = express()
+const PORT = 80
+
+//  Static directory
+app.use(express.static(path.join(__dirname + '/../client')))
+
+//  Handle data parsing
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: "application/vnd.api+json" }))
+
+/*** Routing ***/
+
+//  Todo
+
+//  Start server
+app.listen(PORT, () => {
+	console.log(`App listening on port: ${PORT}`)
+})
